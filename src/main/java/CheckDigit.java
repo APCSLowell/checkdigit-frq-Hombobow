8c22e220735aaeaf0a749788f32d433be448d3af
Hombobow <122179708+Hombobow@users.noreply.github.com>
2024-04-28 13:11:42 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -8,6 +8,16 @@ public class CheckDigit
    public static int getCheck(int num) 
    {  
      /* to be implemented in part (a) */
+     int sum = 0;
+     int number = getNumberOfDigits(num);
+     int i = 7;
+     int j = 1;
+     while (i > 7 - number) {
+       sum += getDigit(num, j) * i;
+       i--;
+       j++;
+     }
+     return sum % 10;
    }
  
   /** Returns true if numWithCheckDigit is valid, or false    
@@ -18,7 +28,8 @@ public class CheckDigit
    */     
    public static boolean isValid(int numWithCheckDigit)    
    {      
-     /* to be implemented in part (b) */    
+     /* to be implemented in part (b) */
+     return (getCheck(numWithCheckDigit / 10) == numWithCheckDigit & 10);
    }    
    
    /** Returns the number of digits in num. */    
